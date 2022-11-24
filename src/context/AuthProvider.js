cc212dfe84ea803f7240cf3770f8b83489984e29
mdaslamalidu <thehometutor2022@gmail.com>
2022-11-24 11:49:40 +0600

    add firebase

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,11 +1,75 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+  signOut,
+  sendPasswordResetEmail,
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
 
-export const AuthContext = createContext("");
-const AuthProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUser = (userInfo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: userInfo,
+    });
+  };
+
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const forgetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  const signinwithgoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const userInfo = {
+    user,
+    createUser,
+    signIn,
+    updateUser,
+    logout,
+    loading,
+    forgetPassword,
+    signinwithgoogle,
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe;
+  }, []);
   return (
-    <div>
-      <h3>Firebase</h3>
-    </div>
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
   );
 };
 
